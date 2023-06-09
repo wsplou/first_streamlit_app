f213cbe9b7467b7aab7a78c64398463dc8d8ee38
Louis <54521494+wsplou@users.noreply.github.com>
2023-06-09 15:24:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit  
 
 streamlit.title('My Parenets New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.header('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Kale, Spinach & Rocket Smoothie')
+streadmlit.header('Hard-Biled Free-Range Egg')
