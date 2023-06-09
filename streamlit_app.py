4e17496316701fa2bd9f5e6a4c3b042403973658
Louis <54521494+wsplou@users.noreply.github.com>
2023-06-09 15:31:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parenets New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Biled Free-Range Egg')
-streamlit.text('🥑🍞Avocade Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Biled Free-Range Egg')
+streamlit.text('🥑🍞 Avocade Toast')
