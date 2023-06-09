b924d2143c40a192836a9f1b7f8fd70c421e3493
Louis <54521494+wsplou@users.noreply.github.com>
2023-06-09 15:31:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Biled Free-Range Egg')
 streamlit.text('🥑🍞 Avocade Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
